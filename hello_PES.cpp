4a3b896cca026f9ce0f3c4799764c9301828dfb7
FedorovJul <fjuluur@mail.ru>
2021-01-17 01:01:14 +0900

    4commit

---
+++ hello_PES.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "fio.h"
+#include <string>
+using namespace std;
+string print_PES()
+{
+    return "Podorozhnay Ekaterina Sergeevna";
+}
---
+++ hello_SMG.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "fio.h"
+#include <string>
+using namespace std;
+
+string print_SMG()
+{
+    return "Samoylova Marya Gennadievna";
+}
